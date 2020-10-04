a7e0a475dac0a810833ff7ce75f7aa01c5dbb508
RetaxMaster <retaxmaster@hotmail.com>
2020-10-03 21:20:10 -0500

    Reparacion del slug

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', "PageController@posts");
-Route::get('blog/{post}', "PageController@post")->name("post");
+Route::get('blog/{post:slug}', "PageController@post")->name("post");
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('posts', 'Backend\PostController')
     ->middleware("auth")
